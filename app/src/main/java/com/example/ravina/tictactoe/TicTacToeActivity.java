838a559bc8d034a99e12f14643620ce8695b5a58
Ravina <ravinakaur@hotmail.com>
2017-12-27 20:44:42 -0800

    Created board with gridlayout and added imageview to each tile

--- app/src/main/java/com/example/ravina/tictactoe/TicTacToeActivity.java
+++ app/src/main/java/com/example/ravina/tictactoe/TicTacToeActivity.java
@@ -2,17 +2,42 @@ package com.example.ravina.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridLayout;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 public class TicTacToeActivity extends AppCompatActivity {
-    GridView board;
+    GridLayout board;
+
+    ImageView sOne;
+    ImageView sTwo;
+    ImageView sThree;
+    ImageView sFour;
+    ImageView sFive;
+    ImageView sSix;
+    ImageView sSeven;
+    ImageView sEight;
+    ImageView sNine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tic_tac_toe);
 
-        board = (GridView) findViewById(R.id.board);
+        board = (GridLayout) findViewById(R.id.board);
+
+
+        sOne = (ImageView) findViewById(R.id.sOne);
+        sTwo = (ImageView) findViewById(R.id.sTwo);
+        sThree = (ImageView) findViewById(R.id.sThree);
+        sFour = (ImageView) findViewById(R.id.sFour);
+        sFive = (ImageView) findViewById(R.id.sFive);
+        sSix = (ImageView) findViewById(R.id.sSix);
+        sSeven = (ImageView) findViewById(R.id.sSeven);
+        sEight = (ImageView) findViewById(R.id.sEight);
+        sNine = (ImageView) findViewById(R.id.sNine);
+
 
     }
+
 }
